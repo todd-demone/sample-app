758060e7185210267b1ef32184658e7d8a12cd4d
Todd Demone <tdemone1@gmail.com>
2017-05-09 12:51:18 -0400

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,9 +7,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   
   test "invalid signup information" do 
     get signup_path
-  
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name:  "",
+      post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
@@ -17,6 +16,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new' 
     assert_select 'div#<CSS id for error explanation>'
     assert_select 'div.<CSS class for error explanation>'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:   "Example User",
+                                          email: "user@example.com",
+                                          password:              "password",
+                                          password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
   end
   
 end
